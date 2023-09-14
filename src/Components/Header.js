533c994111a11d15f1bddfe509be2dc01a25ec60
2023webdevops <123421679+2023webdevops@users.noreply.github.com>
2023-09-14 13:27:33 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='w-full fixed top-0 bg-white border'>
-      <h1 className='font-bold text-center border text-3xl py-2'>Blogs-By Abhishek Sharma</h1>
+      <h1 className='font-bold text-center border text-3xl py-2'>Blogs-By Abhishek Sharma 1</h1>
     </div>
   )
 }
